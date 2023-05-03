d12abb4557a9fa366c724d58881410c050974f4e
Anastasia <nastya.arkanova.98@bk.ru>
2023-05-03 23:45:58 +0500

    add Task3_Array_Difference

---
+++ Task3_Array_Difference/Program.cs
@@ -0,0 +1,19 @@
+﻿double[] array = { 10.3, 12.8, 1.5, 0.8, 1.6 };
+
+double min = array[0];
+double max = array [0];
+foreach (double number in array)
+{
+  if (number < min)
+  {
+    min = number;
+  }
+  if (number > max)
+  {
+    max = number;
+  }
+}
+
+double diff = max - min;
+
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива: {diff}");
